4dcc414fba47e374a20d3d0021d546a7188176c2
Carlos Vital <carloshenrique.vital@gmail.com>
2024-02-25 11:51:42 -0300

    feat: include solution problem 1512 LeetCode

---
+++ Amazon/easy/1512_number_of_good_pairs/js/solution.js
@@ -0,0 +1,54 @@
+/**
+ * Given an array of integers nums, return the number of good pairs.
+ * A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+ *
+ * Example 1:
+ * Input: nums = [1,2,3,1,1,3]
+ * Output: 4
+ * Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+ *
+ * Example 2:
+ * Input: nums = [1,1,1,1]
+ * Output: 6
+ * Explanation: Each pair in the array are good.
+ *
+ * Example 3:
+ * Input: nums = [1,2,3]
+ * Output: 0
+ *
+ * Constraints:
+ * 1 <= nums.length <= 100
+ * 1 <= nums[i] <= 100
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function (nums) {
+  let map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const item = nums[i];
+    if (map.hasOwnProperty(item)) {
+      map[item] += 1;
+    } else {
+      map[item] = 1;
+    }
+  }
+
+  return Object.values(map)
+    .filter((x) => x > 1)
+    .reduce((acc, curr) => {
+      let t = 0;
+      for (let i = curr - 1; i >= 1; i--) {
+        t += i;
+      }
+
+      return acc + t;
+    }, 0);
+};
+
+console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3]));
+console.log(numIdenticalPairs([1, 1, 1, 1]));
+console.log(numIdenticalPairs([1, 2, 3]));
